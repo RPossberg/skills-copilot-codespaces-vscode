2dc158d2d1faa3e36b3ae4865a97aa219491e81c
Merrlin <rpossberg1@nait.ca>
2023-08-26 00:45:40 +0000

    member.js

---
+++ .github/steps/.devcontainer/member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  return {
+    name: 'Skills',
+    description: 'Member',
+    icon: 'https://raw.githubusercontent.com/abranhe/programming-languages-logos/master/src/javascript/javascript_48x48.png',
+    env: {
+      SKILLS: 'JavaScript, TypeScript, Node.js, React, React Native, GraphQL, MongoDB, PostgreSQL, Docker, Kubernetes, AWS, GCP, Azure, Git, GitHub, GitLab, Bitbucket, CircleCI, TravisCI, Jenkins, Heroku, Netlify, Vercel, DigitalOcean, NPM, Yarn, Webpack, Babel, Jest, Mocha, Chai, Cypress, ESLint, Prettier, VSCode, Vim, Linux, macOS, Windows, Bash, Zsh, Fish, Python, Go, Rust, C, C++, C#, Java, Kotlin, Swift, PHP, Ruby, Perl, HTML, CSS, SASS, LESS, Stylus, Tailwind CSS, Bootstrap, Material UI, Semantic UI, Bulma, Foundation, jQuery, Angular, Vue.js, Svelte, Ember, D3.js, Express, Flask, Django, Ruby on Rails, Laravel, Spring, .NET, Flutter, Ionic, Cordova, Electron, Unity, Unreal Engine, Godot, Blender, Figma, Photoshop, Illustrator, InDesign, After Effects, Premiere Pro, Lightroom, XD, Sketch, Framer, InVision, Zeplin, Jira, Trello, Asana, Notion, Confluence, Slack, Discord, Zoom, Google Meet, Microsoft Teams, Skype, WhatsApp, Telegram, Signal, iMessage, SMS, Email, Phone, Zoom, Google Meet, Microsoft Teams, Skype, WhatsApp, Telegram, Signal, iMessage, SMS, Email, Phone'
+    },
+    run: 'echo $SKILLS'
+  }
+}
